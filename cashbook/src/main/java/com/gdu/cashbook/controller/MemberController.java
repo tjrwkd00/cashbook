34fa88b91110025461755d6f6a46c1b5ff758e51
GDJ <GDJ@DESKTOP-UUV0BGU>
2020-05-12 18:20:01 +0900

    1. MemberMapper xml생성 2. MemberMapper 인터페이스 생성 3. MemberService 생성 4. MemberCotroller 와  MemberService 호환

--- cashbook/src/main/java/com/gdu/cashbook/controller/MemberController.java
+++ cashbook/src/main/java/com/gdu/cashbook/controller/MemberController.java
@@ -1,20 +1,28 @@
 package com.gdu.cashbook.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.gdu.cashbook.service.MemberService;
 import com.gdu.cashbook.vo.Member;
 
 @Controller
 public class MemberController {
-	@GetMapping("/addMember")
+	@Autowired
+	private MemberService memberService;
+	
+	@GetMapping("/addMember") // 메소드에만 적용
 	public String addMember() {
 		return "addMember";
 	}
 	
-	@PostMapping("/addMember")
+	@PostMapping("/addMember") // 파라미터 노출  x
 	public String addMember(Member member) { // Command객체 , 도메인객체
-		return "redirect:/";
+		memberService.insertMember(member);
+		System.out.println(member.toString());
+		
+		return "redirect:/index";
 	}
 }
---
+++ cashbook/src/main/java/com/gdu/cashbook/mapper/MemberMapper.java
@@ -0,0 +1,11 @@
+package com.gdu.cashbook.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gdu.cashbook.vo.Member;
+
+@Mapper
+public interface MemberMapper {
+	public int insertMember(Member member);
+}
+	
